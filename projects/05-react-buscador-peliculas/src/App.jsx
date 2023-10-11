122eca3101d3346b2bc49c0a2a4dd0c36517bbba
David Lezcano <lezcadav@gmail.com>
2023-10-11 16:28:34 -0300

    Completado el buscador de peliculas. falta hacer debounce en app.jsx

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -3,18 +3,62 @@ import './App.css'
 //* useRef sirve para tener un objeto de referencia pero que a diferencia de useState, persiste entre los distintos renderizados */
 import { useMovies } from './hooks/useMovies'
 import { Movies } from './components/Movies'
+import { useEffect, useRef, useState } from 'react'
 
 function App() {
-  const { movies:mappedMovies } = useMovies()
   /* const inputRef = useRef() */
+  const [sort, setSort] = useState(false)
+  const {search, updateSearch, error} = useSearch()
+  const { movies, getMovies, loading } = useMovies({ search, sort })
+  
+  const handleSort = () => {
+    setSort(!sort)
+  }
 
+  //formulario de manera no controlada
   const handleSubmit = (event) => {
     event.preventDefault()
-/*     const value = inputRef.current.value */ /* para acceder al valor del inputRef se usa siempre el current porque inputRef es un objeto que puede mutar */
-    const { query } = Object.fromEntries( 
-      new window.FormData(event.target)
-    )
-    console.log(query)
+    getMovies(search)
+    /*const value = inputRef.current.value */ /* para acceder al valor del inputRef se usa siempre el current porque inputRef es un objeto que puede mutar */
+  }
+  
+  //formulario de manera controlada
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    updateSearch(newSearch)
+    getMovies({search: newSearch})
+  }
+  
+  function useSearch() {
+    const [search, updateSearch] = useState('')
+    const [error, setError] = useState(null)
+    const ifFirstInput = useRef(true)
+
+    useEffect(() => {
+      if (ifFirstInput.current) {
+        ifFirstInput.current = search === ''
+        return
+      }
+
+      if(search === '') {
+        setError('No se puede buscar una pelicula vacia')
+        return
+      }
+  
+      if (search.match(/^\d+$/)) {
+        setError('No se puede buscar una película con un número')
+        return
+      }
+  
+      if (search.length < 3) {
+        setError('La búsqueda debe tener al menos 3 caracteres')
+        return
+      }
+  
+      setError(null)
+    }, [search])
+
+    return {search, updateSearch, error}
   }
 
   return (
@@ -22,12 +66,16 @@ function App() {
       <header>
         <h1 className='page'>Buscador de peliculas</h1>
         <form onSubmit={handleSubmit}>
-          <input name='texto' type="text" placeholder='Avengers, Star Wars, The Matrix...'/>
+          <input onChange={handleChange} value={search} name='query' type="text" placeholder='Avengers, Star Wars, The Matrix...'/>
+          <input onChange={handleSort} checked={sort} type="checkbox" name="sort" id="sort" />
           <button type='submit'>Buscar</button>
         </form>
       </header>
+      { error && <p style={{color:'red', textAlign:'center'}}>{error}</p>}
       <main>
-        <Movies movies={mappedMovies} />
+        {
+          loading ? <p>Cargando ... </p> : <Movies movies={movies} />
+        }
       </main>
     </>
   )
