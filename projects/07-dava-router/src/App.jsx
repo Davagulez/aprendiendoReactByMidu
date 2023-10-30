7198e44f59ed02fb21bbf2b93479f0e21af94bbb
Davagulez <davagulez@hotmail.es>
2023-10-30 12:57:03 -0300

    separando las rutas en archivo router

--- projects/07-dava-router/src/App.jsx
+++ projects/07-dava-router/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import HomePage from './pages/Home.jsx'
 import  AboutPage  from './pages/About.jsx'
-import { useEffect } from 'react'
-import { EVENTS } from './const'
+import { Router } from './pages/Router'
 
 
-const routes = [
+const appRoutes = [
   {
     path:'/',
     Component: HomePage
@@ -19,33 +15,14 @@ const routes = [
   }
 ]
 
-function Router({routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1>}) {
-  const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
-  useEffect(() => {
-    const onLocationChange = () => {
-      setCurrentPath(window.location.pathname)  
-    }
-
-    window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
-    window.addEventListener(EVENTS.POPSTATE, onLocationChange)
-
-    return () => {
-      window.removeEventListener(EVENTS.PUSHSTATE, onLocationChange)
-      window.removeEventListener(EVENTS.POPSTATE, onLocationChange)
-    }
-  }, [])
-
-  const Page = routes.find(({ path }) => path === currentPath)?.Component
-  return Page ? < Page /> : < DefaultComponent />
-}
 
 function App() {
   
 
   return (
     <main>
-     <Router routes={routes}/>
+     <Router routes={appRoutes}/>
     </main>
   )
 }
