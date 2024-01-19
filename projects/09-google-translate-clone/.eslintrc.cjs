e7d94e7a5ff7cd4c1e6103245ddd97b80820cd6a
Davagulez <davidfma@hotmail.com>
2024-01-19 15:48:09 -0300

    primeros pasos en el clon de google translate

---
+++ projects/09-google-translate-clone/.eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "ecmaVersion": 'latest',
+        "sourceType": 'module',
+        "project": ['./tsconfig.json', './tsconfig.node.json'],
+        "tsconfigRootDir": "__dirname",
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        'react/react-in-jsx-scope': 'off',
+        
+    }
+}
+4
