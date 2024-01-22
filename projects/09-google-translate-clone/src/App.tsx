173e84103cc6bed4f0f4e024ee2f8e9bb42ab65f
Davagulez <davidfma@hotmail.com>
2024-01-22 16:57:43 -0300

    ✨ Creación del hook de useStore donde se usa reducer con tipado

--- projects/09-google-translate-clone/src/App.tsx
+++ projects/09-google-translate-clone/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useStore } from './hooks/useStore';
 
 function App() {
-
+  const { fromLanguage, setFromLanguage } = useStore()
   return (
     <div className='App'>
       <h1>Google Translate</h1>
+      <button onClick={() => {
+        setFromLanguage('es')
+      }}> Cambiar a Español </button>
+      { fromLanguage }
     </div>
   )
 }
